92229bee6193ca4ee7897eee097af5967558d581
Kwang Woo Lee <kl467@cornell.edu>
2019-12-14 02:57:49 +0900

    MVP + Stretch(1/2)

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,84 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import PeopleCard from './components/PeopleCard';
+import {Spinner, Form, FormGroup, Label, Input} from 'reactstrap';
+import styled from 'styled-components';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [peopleData, setPeopleData] = useState(null);
+  const [inputValue, setInputValue] = useState("");
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(()=> {
+      axios
+        .get('https://swapi.co/api/people')
+        .then(response => {
+          setPeopleData(response.data.results)
+        })
+        .catch(error => {
+          console.log(`Error: ${error}`);
+        })
+  }, [])
+
+  // Diplay loading
+  if (peopleData === null) {
+    return (
+      <div style={{margin: "0 auto"}}>
+        <Spinner type="grow" color="dark" />
+        <Spinner type="grow" color="dark" />
+        <Spinner type="grow" color="dark" />
+      </div>
+    )
+  }
+  console.log(peopleData);
+
+  const CardsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+  `
+
+  // STRETCH
+  const changeHandler = event => {
+    setInputValue(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputValue);
+  }
+
   return (
-    <div className="App">
+    <div className="App" style={{maxWidth: "1280px", margin: "0 auto"}}>
       <h1 className="Header">React Wars</h1>
+      <div className="Search_Form">
+        <Form onSubmit={event => handleSubmit(event)}>
+          <FormGroup>
+            <Label for="SearchValue">Search Your Favorite Character:</Label>
+            <Input
+              type="search"
+              name="search"
+              id="SearchValue"
+              placeholder="ex. Luke Skywalker"
+              onChange={event => changeHandler(event)}
+            />
+          </FormGroup>
+        </Form>
+      </div>
+      <CardsContainer style={{}}>
+        {peopleData.map( (personData, index) => {
+          return <PeopleCard key={index} 
+                             personData = {personData}
+                             displayTF={personData.name.includes(inputValue)}/>
+        })}
+      </CardsContainer>
     </div>
   );
 }
---
+++ starwars/src/components/PeopleCard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, Button
+  } from 'reactstrap';
+import styled from 'styled-components';
+
+function PeopleCard(props) {
+    const {personData, displayTF} = props;
+    console.log(displayTF);
+
+    const CardContainer = styled.div`
+        width: 300px;
+        border: 1px solid #FFE81F;
+        margin: 10px;
+        box-shadow: 0px 0px 8px 2px #000000;
+    `
+
+    return(
+        <CardContainer style={displayTF ? {display: "block"} : {display: "none"}}>
+            <Card>
+                <CardImg top width="100%" src="https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200" alt="Card image cap" style={{width: "100%"}}/>
+                <CardBody>
+                    <CardTitle>{personData.name}</CardTitle>
+                    <CardText>{personData.height}</CardText>
+                    <CardText>{personData.gender}</CardText>
+                    <CardText>{personData.eye_color}</CardText>
+                    <Button>Fake Button</Button>
+                </CardBody>
+            </Card>
+        </CardContainer>
+    )
+}
+
+export default PeopleCard;
